5f37e78912161b887611527ff538fc0bab5c145a
VGrolleau <virginie.grolleau+github@protonmail.com>
2023-11-28 13:11:05 +0100

    Review URL images

--- config/conf.js
+++ config/conf.js
@@ -10,354 +10,354 @@ export let config = {
 export let weatherCodes = {
     0: {
         "weather": "Soleil",
-        "urlImg": "../assets/img/clear.svg"
+        "urlImg": "./assets/img/clear.svg"
     },
     1: {
         "weather": "Peu nuageux",
-        "urlImg": "../assets/img/cloud.svg"
+        "urlImg": "./assets/img/cloud.svg"
     },
     2: {
         "weather": "Ciel voilé",
-        "urlImg": "../assets/img/cloud.svg"
+        "urlImg": "./assets/img/cloud.svg"
     },
     3: {
         "weather": "Nuageux",
-        "urlImg": "../assets/img/cloud.svg"
+        "urlImg": "./assets/img/cloud.svg"
     },
     4: {
         "weather": "Très nuageux",
-        "urlImg": "../assets/img/cloud.svg"
+        "urlImg": "./assets/img/cloud.svg"
     },
     5: {
         "weather": "Couvert",
-        "urlImg": "../assets/img/cloud.svg"
+        "urlImg": "./assets/img/cloud.svg"
     },
     6: {
         "weather": "Brouillard",
-        "urlImg": "../assets/img/haze.svg"
+        "urlImg": "./assets/img/haze.svg"
     },
     7: {
         "weather": "Brouillard givrant",
-        "urlImg": "../assets/img/haze.svg"
+        "urlImg": "./assets/img/haze.svg"
     },
     10: {
         "weather": "Pluie faible",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     11: {
         "weather": "Pluie modérée",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     12: {
         "weather": "Pluie forte",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     13: {
         "weather": "Pluie faible verglaçante",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     14: {
         "weather": "Pluie modérée verglaçante",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     15: {
         "weather": "Pluie forte verglaçante",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     16: {
         "weather": "Bruine",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     20: {
         "weather": "Neige faible",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     21: {
         "weather": "Neige modérée",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     22: {
         "weather": "Neige forte",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     30: {
         "weather": "Pluie et neige mêlées faibles",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     31: {
         "weather": "Pluie et neige mêlées modérées",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     32: {
         "weather": "Pluie et neige mêlées fortes",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     40: {
         "weather": "Averses de pluie locales et faibles",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     41: {
         "weather": "Averses de pluie locales",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     42: {
         "weather": "Averses locales et fortes",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     43: {
         "weather": "Averses de pluie faibles",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     44: {
         "weather": "Averses de pluie",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     45: {
         "weather": "Averses de pluie fortes",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     46: {
         "weather": "Averses de pluie faibles et fréquentes",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     47: {
         "weather": "Averses de pluie fréquentes",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     48: {
         "weather": "Averses de pluie fortes et fréquentes",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     60: {
         "weather": "Averses de neige localisées et faibles",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     61: {
         "weather": "Averses de neige localisées",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     62: {
         "weather": "Averses de neige localisées et fortes",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     63: {
         "weather": "Averses de neige faibles",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     64: {
         "weather": "Averses de neige",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     65: {
         "weather": "Averses de neige fortes",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     66: {
         "weather": "Averses de neige faibles et fréquentes",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     67: {
         "weather": "Averses de neige fréquentes",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     68: {
         "weather": "Averses de neige fortes et fréquentes",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     70: {
         "weather": "Averses de pluie et neige mêlées localisées et faibles",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     71: {
         "weather": "Averses de pluie et neige mêlées localisées",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     72: {
         "weather": "Averses de pluie et neige mêlées localisées et fortes",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     73: {
         "weather": "Averses de pluie et neige mêlées faibles",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     74: {
         "weather": "Averses de pluie et neige mêlées",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     75: {
         "weather": "Averses de pluie et neige mêlées fortes",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     76: {
         "weather": "Averses de pluie et neige mêlées faibles et nombreuses",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     77: {
         "weather": "Averses de pluie et neige mêlées fréquentes",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     78: {
         "weather": "Averses de pluie et neige mêlées fortes et fréquentes",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     100: {
         "weather": "Orages faibles et locaux",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     101: {
         "weather": "Orages locaux",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     102: {
         "weather": "Orages fort et locaux",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     103: {
         "weather": "Orages faibles",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     104: {
         "weather": "Orages",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     105: {
         "weather": "Orages forts",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     106: {
         "weather": "Orages faibles et fréquents",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     107: {
         "weather": "Orages fréquents",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     108: {
         "weather": "Orages forts et fréquents",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     120: {
         "weather": "Orages faibles et locaux de neige ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     121: {
         "weather": "Orages locaux de neige ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     122: {
         "weather": "Orages locaux de neige ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     123: {
         "weather": "Orages faibles de neige ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     124: {
         "weather": "Orages de neige ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     125: {
         "weather": "Orages de neige ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     126: {
         "weather": "Orages faibles et fréquents de neige ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     127: {
         "weather": "Orages fréquents de neige ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     128: {
         "weather": "Orages fréquents de neige ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     130: {
         "weather": "Orages faibles et locaux de pluie et neige mêlées ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     131: {
         "weather": "Orages locaux de pluie et neige mêlées ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     132: {
         "weather": "Orages fort et locaux de pluie et neige mêlées ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     133: {
         "weather": "Orages faibles de pluie et neige mêlées ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     134: {
         "weather": "Orages de pluie et neige mêlées ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     135: {
         "weather": "Orages forts de pluie et neige mêlées ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     136: {
         "weather": "Orages faibles et fréquents de pluie et neige mêlées ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     137: {
         "weather": "Orages fréquents de pluie et neige mêlées ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     138: {
         "weather": "Orages forts et fréquents de pluie et neige mêlées ou grésil",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     140: {
         "weather": "Pluies orageuses",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
     141: {
         "weather": "Pluie et neige mêlées à caractère orageux",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     142: {
         "weather": "Neige à caractère orageux",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     210: {
         "weather": "Pluie faible intermittente",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     211: {
         "weather": "Pluie modérée intermittente",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     212: {
         "weather": "Pluie forte intermittente",
-        "urlImg": "../assets/img/rain.svg"
+        "urlImg": "./assets/img/rain.svg"
     },
     220: {
         "weather": "Neige faible intermittente",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     221: {
         "weather": "Neige modérée intermittente",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     222: {
         "weather": "Neige forte intermittente",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     230: {
         "weather": "Pluie et neige mêlées",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     231: {
         "weather": "Pluie et neige mêlées",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     232: {
         "weather": "Pluie et neige mêlées",
-        "urlImg": "../assets/img/snow.svg"
+        "urlImg": "./assets/img/snow.svg"
     },
     235: {
         "weather": "Averses de grêle",
-        "urlImg": "../assets/img/storm.svg"
+        "urlImg": "./assets/img/storm.svg"
     },
 }
