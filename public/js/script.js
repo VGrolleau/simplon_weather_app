f07be05dbeff92583f11fd7fd26144947b4cc56f
VGrolleau <virginie.grolleau+github@protonmail.com>
2023-12-06 19:05:11 +0100

    Removed unused code and files|Changed style|Reviewed code optimization

--- public/js/script.js
+++ public/js/script.js
@@ -1,65 +1,84 @@
-// Weather API request
-fetch('/weather', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
-})
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        setWeatherData(data);
-    });
+const API_URL = '/weather';
+const IMAGE_API_URL = 'https://source.unsplash.com/1600x900/?';
 
-// Select HTML elements for displaying weather information
-const cityElement = document.querySelector('[data-city]');
-const hourElement = document.querySelector('[data-hour]');
-const iconElement = document.querySelector('[data-icon]');
-const descriptionElement = document.querySelector('[data-description]');
-const realElement = document.querySelector('[data-real]');
-const feelElement = document.querySelector('[data-feel]');
-const arrowElement = document.querySelector('[data-arrow]');
-const speedElement = document.querySelector('[data-speed]');
-const humidityElement = document.querySelector('[data-humidity]');
+const weatherElements = {
+    city: document.querySelector('[data-city]'),
+    hour: document.querySelector('[data-hour]'),
+    icon: document.querySelector('[data-icon]'),
+    description: document.querySelector('[data-description]'),
+    real: document.querySelector('[data-real]'),
+    feel: document.querySelector('[data-feel]'),
+    arrow: document.querySelector('[data-arrow]'),
+    speed: document.querySelector('[data-speed]'),
+    humidity: document.querySelector('[data-humidity]'),
+};
+
+// Function to make the Weather API request
+const getWeatherData = async () => {
+    try {
+        const response = await fetch(API_URL, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        });
+        const data = await response.json();
+        setWeatherData(data);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+};
 
 // Function to format and display weather data
 const setWeatherData = (data) => {
     // Update HTML elements with weather information
+    const { city, hour, icon, description, real, feel, arrow, speed, humidity } = weatherElements;
 
     // City name
-    cityElement.textContent = data.name;
+    city.textContent = data.name;
 
     // Date and time
     const timestamp = data.dt;
-    const date = new Date(timestamp * 1000); // Convert timestamp in ms
-
-    const month = date.getMonth() + 1; // Add one to get actual month
+    const date = new Date(timestamp * 1000);
+    const month = date.getMonth() + 1;
     const day = date.getDate();
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    hourElement.textContent = `${hours}h${(minutes < 10 ? '0' : '') + minutes}`;
+    hour.textContent = `${hours}h${(minutes < 10 ? '0' : '') + minutes}`;
 
     // Update background based on date
     let imgParam = data.name;
-    if ((month === 12 && day >= 1) || (month === 12 && day <= 25)) imgParam = "christmas";
-    if ((month === 12 && day >= 26) || (month === 1 && day <= 5)) imgParam = "new-year";
-    document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${imgParam}')`;
+    if ((month === 12 && day >= 1 && day <= 25) || (month === 12 && day >= 26) || (month === 1 && day <= 5)) {
+        imgParam = (month === 12 && day >= 26) ? "new-year" : "christmas";
+        document.body.style.backgroundImage = `url('${IMAGE_API_URL}${imgParam}')`;
+    }
 
     // Weather icon
-    iconElement.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
 
     // Weather description (first letter capitalized)
-    descriptionElement.innerHTML = data.weather[0].description[0].toUpperCase() + data.weather[0].description.slice(1);
+    description.innerHTML = data.weather[0].description[0].toUpperCase() + data.weather[0].description.slice(1);
 
     // Real and feels-like temperature
-    realElement.textContent = data.main.temp;
-    feelElement.textContent = data.main.feels_like;
+    real.textContent = data.main.temp;
+    feel.textContent = data.main.feels_like;
 
     // Wind direction and speed
-    arrowElement.style.transform = `rotate(${data.wind.deg}deg)`;
-    speedElement.textContent = data.wind.speed;
+    arrow.style.transform = `rotate(${data.wind.deg}deg)`;
+    speed.textContent = data.wind.speed;
 
     // Humidity
-    humidityElement.textContent = data.main.humidity;
+    humidity.textContent = data.main.humidity;
+
+    // Function to refresh page
+    const refreshPage = () => {
+        location.reload(true); // Reload the page ignoring cache
+    };
+
+    // Set a delay of one hour after initial loading
+    setTimeout(refreshPage, 1 * 60 * 60 * 1000); // 60 minutes * 60 seconds * 1000 ms
 };
+
+// Initiate the weather data retrieval
+getWeatherData();
--- services/APIService.js
+++ services/APIService.js
@@ -1,26 +1,53 @@
+// Load environment variables if not in production
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+// Retrieve OpenWeatherMap API key from environment variables
 const appToken = process.env.OPEN_WEATHER_MAP_TOKEN;
+
+// Import necessary modules
 const axios = require('axios');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000; // Use the port defined in environment variables or default to port 3000
 
+// Import variables from config file
 const config = require('../public/js/config.json');
 const cityName = config.city.name;
 const country = config.city.country;
 
+// Validate the presence of the API key
+if (!appToken) {
+    console.error('Missing OpenWeatherMap API token. Please provide a valid token.');
+    process.exit(1);
+}
+
+// Express server configuration
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/weather', (req, res) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${appToken}&lang=${country}&units=metric`;
-    axios({
-        url: url,
-        responseType: 'json'
-    })
-        .then(data => res.json(data.data))
-})
+// Endpoint to retrieve weather data
+app.get('/weather', async (req, res) => {
+    try {
+        // Build the weather API URL with city parameters, API key, language, and units
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${appToken}&lang=${country}&units=metric`;
 
-app.listen(3000, () => {
-    console.log('Server started');
-})
+        // Call the weather API using Axios
+        const response = await axios({
+            url: url,
+            responseType: 'json'
+        });
+
+        // Return weather data as JSON response
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+
+        // In case of an error, return an error response with status 500
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Start the server on the specified port
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
