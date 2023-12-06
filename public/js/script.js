ab726b593ae7e6ee837b89f79dc5e07c4cf2df77
VGrolleau <virginie.grolleau+github@protonmail.com>
2023-12-06 17:14:39 +0100

    Added documentation and adaptative background image|Changed hour element

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+// Weather API request
 fetch('/weather', {
     method: 'GET',
     headers: {
@@ -8,9 +9,10 @@ fetch('/weather', {
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        setWeatherData(data)
-    })
+        setWeatherData(data);
+    });
 
+// Select HTML elements for displaying weather information
 const cityElement = document.querySelector('[data-city]');
 const hourElement = document.querySelector('[data-hour]');
 const iconElement = document.querySelector('[data-icon]');
@@ -21,22 +23,43 @@ const arrowElement = document.querySelector('[data-arrow]');
 const speedElement = document.querySelector('[data-speed]');
 const humidityElement = document.querySelector('[data-humidity]');
 
+// Function to format and display weather data
 const setWeatherData = (data) => {
+    // Update HTML elements with weather information
+
+    // City name
     cityElement.textContent = data.name;
 
+    // Date and time
     const timestamp = data.dt;
     const date = new Date(timestamp * 1000); // Convert timestamp in ms
-    const hour = date.getHours(); // Use Date object method to get hour
-    hourElement.textContent = hour;
 
+    const month = date.getMonth() + 1; // Add one to get actual month
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    hourElement.textContent = `${hours}h${(minutes < 10 ? '0' : '') + minutes}`;
+
+    // Update background based on date
+    let imgParam = data.name;
+    if ((month === 12 && day >= 1) || (month === 12 && day <= 25)) imgParam = "christmas";
+    if ((month === 12 && day >= 26) || (month === 1 && day <= 5)) imgParam = "new-year";
+    document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${imgParam}')`;
+
+    // Weather icon
     iconElement.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
+    // Weather description (first letter capitalized)
     descriptionElement.innerHTML = data.weather[0].description[0].toUpperCase() + data.weather[0].description.slice(1);
 
+    // Real and feels-like temperature
     realElement.textContent = data.main.temp;
     feelElement.textContent = data.main.feels_like;
 
+    // Wind direction and speed
     arrowElement.style.transform = `rotate(${data.wind.deg}deg)`;
     speedElement.textContent = data.wind.speed;
 
+    // Humidity
     humidityElement.textContent = data.main.humidity;
 };
